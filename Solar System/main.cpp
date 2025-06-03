7738aecf12cabe1a21586894bd53449b6288c3ce
ShOOmet14 <walusradoslaw@gmail.com>
2025-06-03 20:01:26 +0200

    Making a first window

--- Solar System/main.cpp
+++ Solar System/main.cpp
@@ -1,6 +1,13 @@
-#include <GLFW/glfw3.h>
+﻿#include <glad/glad.h> //biblioteka obsługująca odnajdywanie funkcji OpenGL, jest zależne od dostawcy sterowników
+#include <GLFW/glfw3.h> //biblioteka obsługująca OpenGL
 
 int main()
 {
-    
+    //inicjalizowanie okna
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    return 0;
 }
