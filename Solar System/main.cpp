dfce1a01ccba8f74a256816045db692225d962be
ShOOmet14 <walusradoslaw@gmail.com>
2025-06-04 11:16:13 +0200

    Made a window with ability to resize it and exit by pressinc escape key on keybord.

--- Solar System/main.cpp
+++ Solar System/main.cpp
@@ -1,13 +1,59 @@
-﻿#include <glad/glad.h> //biblioteka obsługująca odnajdywanie funkcji OpenGL, jest zależne od dostawcy sterowników
-#include <GLFW/glfw3.h> //biblioteka obsługująca OpenGL
+﻿#include <glad/glad.h> // library that supports finding OpenGL functions, that are dependent on the driver manufacturer
+#include <GLFW/glfw3.h> // openGL supporting library
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main()
 {
-    //inicjalizowanie okna
+    // initialization of my window
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Solar System", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // initialiaztion of GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initalize GLAD" << std::endl;
+        return - 1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+
+        // rendering
+
+        // check and call events and swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
